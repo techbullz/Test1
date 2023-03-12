7e598ac8a89ba74d3dd8517e4e2602331e93fe24
J Gilles <devils.adv0kat3@gmail.com>
2023-03-11 22:17:50 -0500

    Cleaned up some code in James_Demo

--- James_Demo/sprite_1.gd
+++ James_Demo/sprite_1.gd
@@ -4,6 +4,7 @@ var speed = 400
 
 func _process(delta):
 	var direction = 0
+
 	# The direction conditions to make the sprite move left & right
 	if Input.is_action_pressed("ui_left"):
 		direction = -1
@@ -13,6 +14,7 @@ func _process(delta):
 	rotation += angular_speed * direction * delta
 	
 	var velocity = Vector2.ZERO
+
 	# The velocity conditions to make the sprite move up & down
 	if Input.is_action_pressed("ui_up"):
 		velocity = Vector2.UP.rotated(rotation) * speed
