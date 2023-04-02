dc7268ed1a110be16813def160c9bbbb58a74e72
J Gilles <jgilles123@gmail.com>
2023-04-02 14:59:27 -0400

    Completed lifebar level using signals.

---
+++ James_Progress/Demo3/using-signals/CustomSignal/LifeBar.gd
@@ -0,0 +1,5 @@
+extends TextureProgressBar
+
+
+func _on_player_health_changed(new_health):
+	value = new_health
---
+++ James_Progress/Demo3/using-signals/ToggleMotion/Godot.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+var speed = 400
+var angular_speed = PI
+
+func _ready():
+	var timer = get_node("Timer")
+	timer.connect("timeout", _on_Timer_timeout)
+
+func _process(delta):
+	rotation += angular_speed * delta
+	var velocity = Vector2.UP.rotated(rotation) * speed
+	position += velocity * delta
+
+
+func _on_button_pressed():
+	set_process(not is_processing()) # Replace with function body.
+
+func _on_Timer_timeout():
+	visible = not visible
