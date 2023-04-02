6e4d92e6aad7b4b9048eeaa07b2faed3a1bd2d30
J Gilles <jgilles123@gmail.com>
2023-04-02 13:46:31 -0400

    Review and practice using the tutorial

---
+++ James_Progress/Demo1/Sprite2D.gd
@@ -0,0 +1,25 @@
+extends Sprite2D
+
+var angular_speed = PI
+var speed = 400
+
+func _process(delta):
+	var direction = 0
+
+	# The direction conditions to make the sprite move left & right
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+
+	rotation += angular_speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+
+	# The velocity conditions to make the sprite move up & down
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+		
+	position += velocity * delta
---
+++ James_Progress/Demo2/sprite_2d.gd
@@ -0,0 +1,22 @@
+extends Sprite2D
+
+var angular_Speed = PI
+var speed = 400.0
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		direction = -1
+	if Input.is_action_pressed("ui_right"):
+		direction = 1
+	
+	rotation += angular_Speed * direction * delta
+	
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity = Vector2.UP.rotated(rotation) * speed
+	if Input.is_action_pressed("ui_down"):
+		velocity = Vector2.DOWN.rotated(rotation) * speed
+		
+	position += velocity * delta
+
